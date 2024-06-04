d60a2024cd4c95fe359f5bfcd716185ac2ffc068
Chetanya hindwan <117354715+chetanya04@users.noreply.github.com>
2024-06-04 12:13:09 +0530

    Add files via upload

---
+++ app/Landing.tsx
@@ -0,0 +1,35 @@
+import "./globals.css";
+
+const Landing: React.FC = () => {
+  return (
+    <div className="landing">
+      <img className="nav" src="Nav.png" alt="img" />
+      <img className="Star" src="Star1.png" alt="img" />
+      <h1 className="Title">Make The Best Financial Decisions</h1>
+      <p>
+        Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
+        faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.
+      </p>
+      <img className="started" src="started.png" alt="img" />
+      <img className="Star2" src="Star1.png" alt="img" />
+      <img className="ladder" src="ladder.png" alt="img" />
+      <img className="spot" src="spot.png" alt="img" />
+      <img className="ml" src="spot.png" alt="" />
+      <img className="background" src="background.png" alt="" />
+      <img className="screen3" src="Screen1.png" alt="img" />
+      <img className="screen2" src="Screen2.png" alt="img" />
+      <img className="screen1" src="screen1.png" alt="img" />
+      <img className="addedspot" src="spot.png" alt="img" />
+      <img className="spotend" src="spot.png" alt="img" />
+      <img className="added" src="added.png" alt="img" />
+      <img src="spot.png" className="endspot1" alt="" />
+      <img className="part3" src="part3.png" alt="img" />
+      <img className="part4" src="part4.png" alt="img" />
+      <img className="endspot2" src="spot.png" alt="img" />
+      <img className="frame" src="frame.png" alt="img" />
+      <img className="endpart" src="endPart.png" alt="img" />
+    </div>
+  );
+};
+
+export default Landing;
